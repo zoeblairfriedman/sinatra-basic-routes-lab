24f7144ff09948937be517ae84e5330607f35d92
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-20 10:43:37 -0500

    commit tree 4db87d1fbf076c1840c8dc4fecf33573823098d2

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is __"
+    end
+
+    get '/hometown' do
+        "My hometown is __"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is __"
+    end
+
 end
